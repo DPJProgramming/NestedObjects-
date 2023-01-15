1481a91bac53aa061b0ab320b591a05a2bf97f62
cheesy d <cheesyd64@hotmail.com>
2023-01-14 18:52:05 -0800

    Created Student and advisor classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects {
+
+	/// <summary>
+	/// Represents an individual college advisor
+	/// </summary>
+	public class Advisor {
+
+		/// <summary>
+		/// Advisor's legal first and last name
+		/// </summary>
+		public string FullName {get; set;}
+
+		/// <summary>
+		/// advisor's school email
+		/// </summary>
+		public string Email { get; set;}
+
+		/// <summary>
+		/// location of advisors office (building and room number)
+		/// </summary>
+		public string OfficeLocation { get; set;}
+	}
+}
